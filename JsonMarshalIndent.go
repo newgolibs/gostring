b82743a3b62634ff767efcb41d220c95a5c2bc1e
xltxlm <xltxlm@qq.com>
2020-07-31 16:55:51 +0800

    message:(001697)

--- JsonMarshalIndent.go
+++ JsonMarshalIndent.go
@@ -2,9 +2,15 @@ package gostring
 
 import (
 	"encoding/json"
+	"fmt"
+	"runtime"
 )
 
 func JsonMarshalIndent(v interface{}) string {
-	bytes, _ := json.MarshalIndent(v, "\t", "")
+	bytes, err := json.MarshalIndent(v, "\t", "")
+	if err != nil {
+		_, file, line, _ := runtime.Caller(0)
+		panic(fmt.Sprintf("\033[41;36merr:%+v %+v:%+v\033[0m\n", []interface{}{err}, file, line))
+	}
 	return string(bytes)
 }
